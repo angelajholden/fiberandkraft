6ee1fc7dcc8cd233c0a97ab3925bcc6bd0d38067
Angela Holden <angelajholden@gmail.com>
2024-04-17 13:30:09 -0500

    updating navigation and styles

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,34 @@ function updateCopyright() {
 	}
 }
 
+function youAreHereNavigation() {
+	const links = document.querySelectorAll(".nav_inner_list_item");
+	const cartLink = document.querySelector(".cart a");
+	const cartAttr = cartLink.getAttribute("href");
+	const currentPath = window.location.pathname;
+
+	if (currentPath === cartAttr) {
+		cartLink.classList.add("active");
+	}
+
+	links.forEach((link) => {
+		const linkPath = link.getAttribute("href");
+		if (linkPath === currentPath) {
+			link.classList.add("active");
+		}
+	});
+}
+
+function toggleMenu() {
+	const menu = document.querySelector(".menu");
+	const nav = document.querySelector(".nav");
+	menu.addEventListener("click", function () {
+		nav.classList.toggle("active");
+	});
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	updateCopyright();
+	youAreHereNavigation();
+	toggleMenu();
 });
