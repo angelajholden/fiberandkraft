8ae4849030522898c05ffe4f1704a3058ff7ca39
Angela Holden <angelajholden@gmail.com>
2024-04-28 13:39:56 -0500

    moving html and scss

--- fiberandkraft/src/app/app-routing.module.ts
+++ fiberandkraft/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'product', component: ProductsComponent },
+  { path: 'product/:end-point', component: ProductPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- fiberandkraft/src/app/app.module.ts
+++ fiberandkraft/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    ProductPageComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
