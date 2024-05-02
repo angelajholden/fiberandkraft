b8fa100d98c12a0f9f89b6a2a2cb9173c4d3e083
Angela Holden <angelajholden@gmail.com>
2024-05-02 16:03:22 -0500

    updates

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static("./dist/fiber-and-kraft"));
+app.use(express.static(__dirname + "/dist/fiberandkraft"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/fiber-and-kraft/" })
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/fiberandkraft/index.html"));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
